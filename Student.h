08b00d279828dcc014514caa84558e63694eac11
sl0bby <danik.zaycev2020@gmail.com>
2025-04-07 17:38:44 +0300

    Use Struction option

--- Student.h
+++ Student.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Student {
@@ -7,4 +8,14 @@ public:
 	int age;
 	double mark;
 	bool alive;
+
+	string getString() {
+		string s = "Name: " + name + "\n";
+		s += "Age: " + to_string(age) + "\n";
+		s += "Mark: " + to_string(mark) + "\n";
+		s += "Alive: ";
+		s += (alive ? "Yes" : "No");
+
+		return s;
+	}
 };
